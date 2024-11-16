d4e3a0ed723b8581086bd795cb038fddb3305e4e
ShafiqTanim <shafiqullah715@gmail.com>
2024-11-17 00:29:33 +0600

    Booking Table edited.

--- database/migrations/2024_10_25_131715_create_bookings_table.php
+++ database/migrations/2024_10_25_131715_create_bookings_table.php
@@ -17,13 +17,13 @@ return new class extends Migration
             $table->foreignId('room_list_id')->nullable()->constrained()->onDelete('set null');
             $table->string('contact_no');
             $table->date('check_in_date');
-            $table->date('check_out_date');
+            $table->date('check_out_date')->nullable();
             $table->string('number_of_guest_adult', 2);
             $table->string('number_of_guest_child', 2);
-            $table->decimal('total_amount', 10, 2);
+            $table->decimal('total_amount', 10, 2)->nullable();
             $table->decimal('discount', 10, 2)->nullable();
             $table->decimal('vat', 10, 2)->nullable();
-            $table->integer('status')->default(0)->comment('0 active, 1 cancelled');
+            $table->integer('status')->default(0)->comment('0 active, 1 cancelled')->nullable();
             $table->text('cancel_reason')->nullable();
             $table->timestamps();
         });
